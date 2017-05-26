131e945cbcdb5e0029d935180ec2aa896df6a877
Roland Puhwein <roland.puhwein@ebcont.com>
2017-05-26 17:11:12 +0200

    added check_synced_from and joomlaID vs id logic to tours

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ToursController < Api::V1::BaseController
-  before_action :set_tour, only: [:show, :edit, :update, :destroy]
+  before_action :set_tour, only: [:edit, :update, :destroy]
+  before_action :set_tour_show, only: [:show]
+  before_action :check_synced_from, only: [:edit, :update, :destroy]
 
   # GET /tours
   # GET /tours.json
@@ -28,11 +30,11 @@ class Api::V1::ToursController < Api::V1::BaseController
 
     respond_to do |format|
       if @tour.save
-        format.html { redirect_to @tour, notice: 'Tour was successfully created.' }
-        format.json { render :show, status: :created, location: @tour }
+        format.html {redirect_to @tour, notice: 'Tour was successfully created.'}
+        format.json {render :show, status: :created, location: @tour}
       else
-        format.html { render :new }
-        format.json { render json: @tour.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @tour.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +44,12 @@ class Api::V1::ToursController < Api::V1::BaseController
   def update
     respond_to do |format|
       if @tour.update(tour_params)
-        format.html { redirect_to @tour, notice: 'Tour was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tour }
+        format.html {redirect_to @tour, notice: 'Tour was successfully updated.'}
+
+        format.json {render :show, status: :ok, location: @tour}
       else
-        format.html { render :edit }
-        format.json { render json: @tour.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @tour.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,19 +59,47 @@ class Api::V1::ToursController < Api::V1::BaseController
   def destroy
     @tour.destroy
     respond_to do |format|
-      format.html { redirect_to tours_url, notice: 'Tour was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to tours_url, notice: 'Tour was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tour
+  # Use callbacks to share common setup or constraints between actions.
+  # checks for the app that requests and uses the correct id
+  def set_tour_show
+    if params[:syncedFrom] == '1'
+      @tour = Tour.where("joomlaID = #{ params[:id] }").first
+    else
       @tour = Tour.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def tour_params
-      params.require(:tour).permit(:joomlaID, :name, :maxAttendees, :price, :startDate, :endDate, :createdFrom, :changedFrom, :syncedFrom, :deleteFlag)
+  end
+
+  # checks for the app that requests and uses the correct id
+  def set_tour
+
+    if tour_params["syncedFrom"] == 1
+      @tour = Tour.where("joomlaID = #{params[:id]}").first
+    else
+      @tour = Tour.find(params[:id])
     end
+
+  end
+
+  # checks if app with sycnedFrom==2 wants to change entry from web and is therefore not allowed
+  def check_synced_from
+    if tour_params["syncedFrom"] == 2
+      if @tour.syncedFrom == 1
+        render json: {error: "you are not allowed to overwrite the web because the web is the ruler of the world"}, :status => :forbidden
+      end
+    end
+
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def tour_params
+    params.require(:tour).permit(:joomlaID, :name, :maxAttendees, :price, :startDate, :endDate, :createdFrom, :changedFrom, :syncedFrom, :deleteFlag)
+  end
 end
