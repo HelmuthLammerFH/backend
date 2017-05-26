ec28a9d1d64067426e69049da3e6d15a4093a920
Roland Puhwein <roland.puhwein@ebcont.com>
2017-05-26 18:06:06 +0200

    added global functions to use in all api controllers and testet it with tours

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::BaseController < ApplicationController
   def destroy_session
     request.session_options[:skip] = true
   end
+
+  def set_variable(table, decider, id)
+    if decider == 1
+      return table.where("joomlaID = #{ id }").first
+    else
+      return table.find(id)
+    end
+  end
+
+  def global_check_synced_from(model, syncedFrom)
+    if syncedFrom == 2
+      if model.syncedFrom == 1
+        render json: {error: "you are not allowed to overwrite the web because the web is the ruler of the world"}, :status => :forbidden
+      end
+    end
+  end
 end
--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -68,34 +68,18 @@ class Api::V1::ToursController < Api::V1::BaseController
   private
   # Use callbacks to share common setup or constraints between actions.
   # checks for the app that requests and uses the correct id
-  def set_tour_show
-    if params[:syncedFrom] == '1'
-      @tour = Tour.where("joomlaID = #{ params[:id] }").first
-    else
-      @tour = Tour.find(params[:id])
-    end
-
+  def set_tour
+    @tour = set_variable(Tour, params[:syncedFrom], params[:id])
   end
 
   # checks for the app that requests and uses the correct id
-  def set_tour
-
-    if tour_params["syncedFrom"] == 1
-      @tour = Tour.where("joomlaID = #{params[:id]}").first
-    else
-      @tour = Tour.find(params[:id])
-    end
-
+  def set_tour_show
+    @tour = set_variable(Tour, params[:syncedFrom], params[:id])
   end
 
-  # checks if app with sycnedFrom==2 wants to change entry from web and is therefore not allowed
+  # checks for the app that requests and uses the correct id
   def check_synced_from
-    if tour_params["syncedFrom"] == 2
-      if @tour.syncedFrom == 1
-        render json: {error: "you are not allowed to overwrite the web because the web is the ruler of the world"}, :status => :forbidden
-      end
-    end
-
+    global_check_synced_from(@tour, tour_params['syncedFrom'])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
