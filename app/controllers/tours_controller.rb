633a67a8146cbcf0be40e65f86946748a2a2ce5b
Roland Puhwein <roland.puhwein@ebcont.com>
2017-05-28 16:41:13 +0200

    updated all views from api controllers with the new ones and fixed tours jsonbuilder

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -69,6 +69,6 @@ class ToursController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tour_params
-      params.require(:tour).permit(:joomlaID, :name, :maxAttendees, :price, :startDate, :endDate, :createdFrom, :changedFrom, :syncedFrom, :deleteFlag, :status_id, :TourGuide_id)
+      params.require(:tour).permit(:joomlaID, :name, :maxAttendees, :price, :startDate, :endDate, :createdFrom, :changedFrom, :syncedFrom, :deleteFlag, :status_id, :Tour_Guide_id)
     end
 end
