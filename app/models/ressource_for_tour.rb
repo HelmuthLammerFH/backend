926e5ed4aa14714366719739fbd8d01502aa71fc
Roland Puhwein <roland.puhwein@ebcont.com>
2017-05-29 21:00:14 +0200

    fixed typos?

--- app/models/ressource_for_tour.rb
+++ app/models/ressource_for_tour.rb
@@ -1,9 +1,6 @@
 class RessourceForTour < ApplicationRecord
   belongs_to :RessourceTyp
-<<<<<<< HEAD
   belongs_to :Tour
-=======
-  
+
   validates :id, :presence => true
->>>>>>> 7f3b16426d4a68dcd5142cf6587148027745addf
 end
