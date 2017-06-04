d31c95904adba2d1fb75ae4f74b39b18903c8ee2
patRiesa <patrickrieser@msn.com>
2017-06-04 19:06:46 +0200

    Added new Chart with data from code

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,4 +1,12 @@
 class ReportController < ApplicationController
   def index
+    @data = [
+        ["Washington", "1789-04-29", "1797-03-03"],
+        ["Adams", "1797-03-03", "1801-03-03"],
+        ["Jefferson", "1801-03-03", "1809-03-03"]
+    ]
+
+    
   end
+
 end
