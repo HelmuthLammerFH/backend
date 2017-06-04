2c16fed33cddb8f4785e8f5c84e856c010da6a96
patRiesa <patrickrieser@msn.com>
2017-06-04 20:49:38 +0200

    Added graphs to Reports section

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,12 +1,37 @@
 class ReportController < ApplicationController
   def index
-    @data = [
-        ["Washington", "1789-04-29", "1797-03-03"],
-        ["Adams", "1797-03-03", "1801-03-03"],
-        ["Jefferson", "1801-03-03", "1809-03-03"]
+    @tourGuideData = [
+        ['Hubert', 12],
+        ['Franz', 22],
+        ['Herbert', 1],
+        ['Walter', 4],
+        ['Thomas', 7]
     ]
 
-    
+    @linchartData = [
+        ['12.12.2016', '133'],
+        ['14.12.2016', '143'],
+        ['16.12.2016', '323'],
+        ['18.12.2016', '893']
+    ]
+
+    #@CustomersPerTour  = Tour.joins(:Tourguide)
+
+    #@CustomersPerTour = Tour.joins(:CustomerInTour)
+
+
+    # ======================
+    #  Anzahl ned korrekt :((
+    #   @CustomersPerTour = Tour.joins("INNER JOIN customer_in_tours as cit ON cit.tour_id = tours.id ").group(:id).select('count(tours.id) AS leit, tours.name')
+    # ======================
+
+    # CustomerInTour.group(:tour_id).count #works
+
+
+    puts "asdksalladlkdadkjlkajdöldöjsajdsadjsakdjsadjdsajdjdalk"
+    puts json: @CustomersPerTour
+
+
   end
 
 end
