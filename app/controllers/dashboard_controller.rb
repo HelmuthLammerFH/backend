87e20ee77441fc2a8a660ef4bc79fa9659f509c0
Roland Puhwein <roland.puhwein@ebcont.com>
2017-06-05 18:35:57 +0200

    save

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+
+    @tours = Tour.includes(Tourguide: [ :user]).all
+    #@tours = Tour.all
+    @tourguides = @tours
+    puts json: @tours
+    puts json: @tourguides
   end
 end
